cf54f30ee2c49b1ae55c75e254396ef3a6b87c9b
SharminAkterLiza <s.a.liza6181@gmail.com>
2024-05-09 10:25:25 +0600

    delete from cart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -54,6 +54,14 @@ async function run() {
     const result = await cartCollection.insertOne(cartItem);
     res.send(result);
   })
+
+  app.delete('/carts/:id', async(req,res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
